fcc60a6d11befecf9f676b8317729d007113bcff
juanzeballos <juanzeballos.jmz@gmail.com>
2026-05-21 16:28:30 -0300

    Prueba

--- backend/src/main/java/com/taller/sae/controller/SpaController.java
+++ backend/src/main/java/com/taller/sae/controller/SpaController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class SpaController {
 
-    @RequestMapping(value = { "/", "/{path:[^\\.]*}", "/{path:[^\\.]*}/**" })
+    @RequestMapping(value = { "/", "/{path:[^\\.]*}" })
     public String spa() {
         return "forward:/index.html";
     }
